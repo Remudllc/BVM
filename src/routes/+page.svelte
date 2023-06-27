11bbfa3cf392aeba10a55aeb80eb4f619d289df2
cody titmus <cody.titmus@lvt.com>
2023-06-26 22:35:35 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<body>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</body>
+
+<style>
+	h1,
+	p {
+		color: white;
+	}
+	body {
+		background: black;
+	}
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import netlify from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: netlify(),
+		target: '#svelte'
+	}
+};
+
+export default config;
