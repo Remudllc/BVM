99e1c69edffebe1343437e9e20380f02c2ff5e15
cody titmus <cody.titmus@lvt.com>
2023-06-27 17:52:56 -0600

    updated to have basic layout of main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
-<div class="title-bar">
-	<h1>Time to Clean Up? We Can Help</h1>
+<div class="app">
+	<div class="title-bar">
+		<img class="logo" src="/logo.png" />
+		<h1>Time to Clean Up? We Can Help</h1>
+		<img class="logo" src="/logo.png" />
+	</div>
+	<div class="main-page">
+		<div>
+			<p>Exterior</p>
+		</div>
+		<img class="pigs" src="/pigs.png" alt="dirty-pigs" />
+		<div>
+			<p>Interior</p>
+		</div>
+	</div>
 </div>
 
 <style>
@@ -7,14 +20,25 @@
 		display: grid;
 		place-items: center;
 		border-bottom: 1px solid yellow;
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+	.main-page {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		place-items: center;
+	}
+	.logo {
+		width: 10rem;
+	}
+	.pigs {
+		/* width: 70%; */
 	}
 	h1 {
 		font-size: 2em;
 		text-align: center;
 		font-weight: normal;
 	}
-	h1,
-	div {
+	.app {
 		color: white;
 	}
 </style>
