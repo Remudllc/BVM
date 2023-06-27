ee75caaa036f7967321ae596b696f23a3976767f
cody titmus <cody.titmus@lvt.com>
2023-06-26 23:15:35 -0600

    removed netlify toml

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,9 @@
-<body>
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</body>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style>
 	h1,
 	p {
 		color: white;
-	}
-	body {
-		background: black;
 	}
 </style>
